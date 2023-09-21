49400b6b0315a3de25cef0ca9f7ce464ca61df2a
luca510566 <luca510566@ys-infoware.com>
2023-09-21 16:00:41 +0800

    deploy 2023-09-21 16:00:40

--- gojdb/SchemaDesign.go
+++ gojdb/SchemaDesign.go
@@ -6,7 +6,7 @@ import (
 
 func (db GOJDB) UpdateTable(table map[string]interface{}) {
 	db.ParaClear()
-	sqlstring := fmt.Sprintf("Select object_id from sys.objects where name = %s", table["TableName"])
+	sqlstring := fmt.Sprintf("Select object_id from sys.objects where name = '%s'", table["TableName"])
 	fmt.Println(sqlstring)
 	result, err := db.Scalar(sqlstring, nil)
 	if err != nil {
