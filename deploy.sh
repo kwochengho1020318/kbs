5b0ffd6c3a2d9fcdc998d8d0469ab1b4be2ed837
luca510566 <luca510566@ys-infoware.com>
2023-09-21 10:37:04 +0800

    deploy 2023-09-21 10:37:04

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git add .
 git commit -m "deploy $current_datetime"
 git push origin master
 ssh ys@192.168.2.228 "cd /home/ys/ai4u-api-main/ ;git pull origin master"
-expect "ys@192.168.2.228's password"
+expect "ys@192.168.2.228's password:"
 
 send "$password\r"
 
