39296ac160f5105f639e1db0435d03bd53964d00
luca510566 <luca510566@ys-infoware.com>
2023-09-21 10:54:13 +0800

    deploy 2023-09-21 10:54:13

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ password = "pass"
 git add .
 git commit -m "deploy $current_datetime"
 git push origin master
-ssh ys@192.168.2.228 "cd /home/ys/ai4u-api-main/ ;git pull origin master"
+ssh ys@192.168.2.228 "cd /home/ys/ai4u-api-main/ ;git pull origin master; docker build -t lucatest .; docker-compose up -d "
