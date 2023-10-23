8d36536df16a1100cba0687ffe8c63260a88250e
luca510566 <luca510566@ys-infoware.com>
2023-10-23 16:41:11 +0800

    chain

--- api/HtmlSource.go
+++ api/HtmlSource.go
@@ -2,25 +2,26 @@ package api
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
 	"main/config"
 	"net/http"
 )
 
 func InsertHtml(w http.ResponseWriter, r *http.Request) {
+	var params map[string]string
 	config := config.NewConfig()
-	prompt := r.URL.Query().Get("prompt")
-	uid := r.URL.Query().Get("uid")
-	table_name := r.URL.Query().Get("table_name")
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
+	decoder := json.NewDecoder(r.Body)
+	decoder.Decode(&params)
+
+	url := params["url"]
+	uid := params["uid"]
+	table_name := params["table_name"]
+	body := params["prompt"]
 
 	chaturl := config.App.ChatUrl
-	targeturl := chaturl + "api/chat/ExtractHtml?prompt" + prompt + "&uid=" + uid
-	payload := body
+	targeturl := chaturl + "api/chat/ExtractHtml?prompt=" + body + "&uid=" + uid
+	payload := []byte(url)
 	resp, err := http.Post(targeturl, "application/text", bytes.NewReader(payload))
 	result, _ := io.ReadAll(resp.Body)
 	if err != nil {
